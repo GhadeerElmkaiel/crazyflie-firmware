78b6251e6352db8f3121a33458e68d3418b76be5
Ghadeer <ghadeer.elmkaiel@gmail.com>
2025-10-14 11:36:11 +0200

    Changed flap angles to near hover configureation when Optitrack communications is lost

--- src/modules/src/controller_floaty.c
+++ src/modules/src/controller_floaty.c
@@ -468,15 +468,17 @@ void controllerFloaty(floaty_control_t *control, setpoint_t *setpoint,
           // control->flap_1 = ext_ctrl_m1;
         }
         else{
+          // What happens when the communication is lost
           if(state->connectedToOffboard==false){
             // control->flap_1 = ext_ctrl_m1;
             // control->flap_2 = ext_ctrl_m2;
             // control->flap_3 = ext_ctrl_m3;
             // control->flap_4 = ext_ctrl_m4;
-            control->flap_1 = FLAP_1_HOVER_ANGLE+0.1;
-            control->flap_2 = FLAP_2_HOVER_ANGLE-0.1;
-            control->flap_3 = FLAP_3_HOVER_ANGLE+0.1;
-            control->flap_4 = FLAP_4_HOVER_ANGLE-0.1;
+            // // This is a safer configuration for landing
+            control->flap_1 = FLAP_1_HOVER_ANGLE+0.05;
+            control->flap_2 = FLAP_2_HOVER_ANGLE-0.05;
+            control->flap_3 = FLAP_3_HOVER_ANGLE+0.05;
+            control->flap_4 = FLAP_4_HOVER_ANGLE-0.05;
           }
         }
         // if(manual==3){
