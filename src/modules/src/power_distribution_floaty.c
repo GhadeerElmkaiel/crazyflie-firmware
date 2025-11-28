b16c19501721926e42af1f5d01890ec1a840e251
Ghadeer <ghadeer.elmkaiel@gmail.com>
2025-11-28 13:54:14 +0100

    Working models and controllers for the algorithm

--- src/modules/src/power_distribution_floaty.c
+++ src/modules/src/power_distribution_floaty.c
@@ -56,12 +56,6 @@
 // static int16_t motShift4 = 4400;
 
 // // Floaty V3
-// int16_t motShift1 = -3600;
-// int16_t motShift2 = -5800;
-// int16_t motShift3 = -9000;
-// int16_t motShift4 = 6200;
-
-// Floaty V4
 int16_t motShift1 = -3600;
 int16_t motShift2 = -7000;
 int16_t motShift3 = 4300;
